858b43766ce6fc1828350e6eefbff875d8b6c6d3
servicioscognitivos <joselyn.m.sevilla.v@pucesa.edu.ec>
2018-11-29 08:57:01 -0500

    Servicos cognitivos-emocion

---
+++ App5/App5/App5/Class/ServiceEmotions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Linq;
+using Microsoft.ProjectOxford.Emotion;
+using System.Threading.Tasks;
+
+namespace App5.Class
+{
+   public class ServiceEmotions
+    {
+        static string key = "65b532e4e8e34e518750ef7212ce626d";
+        
+        public static async Task<Dictionary<string, float>> GetEmotions(Stream stream)
+        {
+            EmotionServiceClient cliente = new EmotionServiceClient(key);
+            var emociones = await cliente.RecognizeAsync(stream);
+
+            if (emociones == null || emociones.Count() == 0)
+                return null;
+
+            return emociones[0].Scores.ToRankedList().ToDictionary(x => x.Key, x => x.Value);
+        }
+
+    }
+}
